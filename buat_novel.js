aeaecfbeb54197ceeca56b2751c86fb9965b419d
argapurab43-maker <argapurab43@gmail.com>
2026-05-15 11:16:37 +0800

    buat_novel.js

--- buat_novel.js
+++ buat_novel.js
@@ -1,10 +1,40 @@
-function kembali(){
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+
+import {
+  getFirestore,
+  collection,
+  addDoc
+}
+from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
+
+import {
+  getAuth
+}
+from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLu1POb4qzy6yqZ9gz6xHu4u_HlJ8BMMY",
+  authDomain: "argaa-web.firebaseapp.com",
+  projectId: "argaa-web",
+  storageBucket: "argaa-web.firebasestorage.app",
+  messagingSenderId: "85804841719",
+  appId: "1:85804841719:web:df2129e8586a445307f1f2"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+const auth = getAuth(app);
+
+window.kembali = function(){
 
   window.location.href = "novel.html";
 
 }
 
-function publishNovel(){
+window.publishNovel = async function(){
 
   let judul =
   document.getElementById("judul").value;
@@ -14,14 +44,45 @@ function publishNovel(){
 
   if(judul === "" || isi === ""){
 
-    alert("😭 Judul dan isi novel harus diisi");
+    alert(
+      "😭 Judul dan isi novel harus diisi"
+    );
 
     return;
 
   }
 
-  alert(
-    "🔥 Novel berhasil dipublish (sementara demo dulu 😎)"
-  );
+  try{
+
+    const user = auth.currentUser;
+
+    await addDoc(
+      collection(db, "novels"),
+      {
+        judul: judul,
+        isi: isi,
+        penulis: user.email,
+        createdAt: Date.now()
+      }
+    );
+
+    alert(
+      "🔥 Novel berhasil dipublish"
+    );
+
+    window.location.href =
+    "novel.html";
+
+  }
+
+  catch(error){
+
+    alert(
+      "❌ Error: " + error.message
+    );
+
+    console.log(error);
+
+  }
 
 }
